56bcec29bb4929cdb5e97c77e7e2c66fdf044b3b
waleokeofficial <waleokeofficial@gmail.com>
2022-09-17 23:02:09 +0100

    Navbar corrected. Done. 99%

--- modern-bank-react-app/src/components/Navbar.jsx
+++ modern-bank-react-app/src/components/Navbar.jsx
@@ -1,15 +1,3 @@
-// import React from 'react'
-
-// const Navbar =()=> {
-//     return (
-//         <div>
-//             Navbar component
-//         </div>
-//     )
-// }
-
-// export default Navbar
-
 import { logo, close, menu } from '../assets';
 import {navLinks} from '../constants'
 import { useState } from 'react';
@@ -21,23 +9,25 @@ const Navbar =()=> {
         <nav className='w-full py-6 flex justify-between items-center flex-1 navbar'>
             <img src={logo} alt="hubobank" className="w-[124px] h-[32px]"/>
             
-            <ul className='list-none sm:flex hidden justify-between items-center'>
+            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
                 {navLinks.map((navigg, index) => (
-                    <li className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`} key={navigg.id} >
-                        <a href={`#${navigg.id}`}>{navigg.title}</a>
+                    <li key={navigg.id} className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`} key={navigg.id} >
+                        <a href={`#${navigg.id}`}>
+                            {navigg.title}
+                        </a>
                     </li>
                 ))}
             </ul>
 
-            <div className="sm:none flex flex-1 justify-end items-center">
-                    <img className='w-[24px] h-[24px] object-contain' src={toggle ? close : menu} alt="menu" onClick={()=> setToggle((prev) => (!prev))} />
+             <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img className='w-[24px] h-[24px] object-contain' src={toggle ? close : menu} alt="menu" onClick={()=> setToggle((prev) => !prev)} />
             </div>
-            
+
             <div className=
-            {`${toggle ? 'flex' : 'hidden'} sidebar p-5 top-20 right-0 bg-black-gradient mx-2 my-4 rounded-xl absolute min-w-[140px]`}>
-                <ul className='list-none flex flex-col flex-1 column justify-between items-center'>
+            {`${toggle ? 'flex' : 'hidden'} sidebar p-6  top-20 right-0 bg-black-gradient my-2 mx-4 rounded-xl absolute min-w-[140px]`}>
+                <ul className='list-none flex flex-col flex-1 column justify-end items-center'>
                 {navLinks.map((navigg, index) => (
-                    <li className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`} key={navigg.id} >
+                    <li key={navigg.id} className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`} key={navigg.id} >
                         <a href={`#${navigg.id}`}>{navigg.title}</a>
                     </li>
                 ))}
